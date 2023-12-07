468ce19e86cf2b67f8d2fecf62b39f2dae5d5cff
teesha2201 <teesha2201@gmail.com>
2023-12-07 18:31:31 +0530

    commit

--- src/Home_compo/HomeContainer.js
+++ src/Home_compo/HomeContainer.js
@@ -5,7 +5,7 @@ import axios from "axios";
 
 const HomeContainer =()=>{
     const [home,setHome] = useState([])
- 
+    const [cart,setCart] = useState()
 useEffect(()=>{
  
       axios.get('https://udemy-backend-server.onrender.com/udemy/getdatafromstore')
@@ -15,6 +15,25 @@ useEffect(()=>{
           .catch((err)=>console.log(err))
     
     },[])
+    useEffect(()=>{
+        axios.get('https://udemy-backend-server.onrender.com/udemy/getaddtocart')
+        .then(res=>setCart(res.data))
+        .catch((err)=>console.log(err))
+    },[]) 
+    // console.log(cart)
+    const handleClick = async(item)=>{
+        const findcart =  cart && cart.find((items)=>items.id===item.id);
+        console.log(findcart)
+        if(findcart){
+            alert('Item is already in cart')
+        }
+       else{
+        console.log(item.id)
+        await axios.post("https://udemy-backend-server.onrender.com/udemy/addtocart",item)
+        alert("Item has successfully added in your cart")
+
+       }
+}      
 
     return(
         < >
@@ -184,7 +203,7 @@ useEffect(()=>{
                                                         </div> 
 
                                                         <div className="hoveraddtocart">
-                                                            <button className="addtocartbutton">
+                                                            <button className="addtocartbutton" onClick={()=>handleClick(item)}>
                                                                 Add to cart
                                                             </button>
                                                             <div className="wishlist">
@@ -264,7 +283,7 @@ useEffect(()=>{
                                                         </div>
 
                                                         <div className="hoveraddtocart">
-                                                            <button className="addtocartbutton">
+                                                            <button className="addtocartbutton" onClick={()=>handleClick(item)}>
                                                                 Add to cart
                                                             </button>
                                                             <div className="wishlist">
@@ -344,7 +363,7 @@ useEffect(()=>{
                                                         </div> 
 
                                                         <div className="hoveraddtocart">
-                                                            <button className="addtocartbutton">
+                                                            <button className="addtocartbutton" onClick={()=>handleClick(item)}>
                                                                 Add to cart
                                                             </button>
                                                             <div className="wishlist">
@@ -423,7 +442,7 @@ useEffect(()=>{
                                                         </div> 
 
                                                         <div className="hoveraddtocart">
-                                                            <button className="addtocartbutton">
+                                                            <button className="addtocartbutton" onClick={()=>handleClick(item)}>
                                                                 Add to cart
                                                             </button>
                                                             <div className="wishlist">
@@ -568,7 +587,7 @@ useEffect(()=>{
                                                 </div> 
 
                                                 <div className="hoveraddtocart">
-                                                    <button className="addtocartbutton">
+                                                    <button className="addtocartbutton" onClick={()=>handleClick(item)}>
                                                         Add to cart
                                                     </button>
                                                     <div className="wishlist">
@@ -647,7 +666,7 @@ useEffect(()=>{
                                                 </div> 
 
                                                 <div className="hoveraddtocart">
-                                                    <button className="addtocartbutton">
+                                                    <button className="addtocartbutton" onClick={()=>handleClick(item)}>
                                                         Add to cart
                                                     </button>
                                                     <div className="wishlist">
@@ -726,7 +745,7 @@ useEffect(()=>{
                                                 </div> 
 
                                                 <div className="hoveraddtocart">
-                                                    <button className="addtocartbutton">
+                                                    <button className="addtocartbutton" onClick={()=>handleClick(item)}>
                                                         Add to cart
                                                     </button>
                                                     <div className="wishlist">
@@ -805,7 +824,7 @@ useEffect(()=>{
                                                 </div> 
 
                                                 <div className="hoveraddtocart">
-                                                    <button className="addtocartbutton">
+                                                    <button className="addtocartbutton" onClick={()=>handleClick(item)}>
                                                         Add to cart
                                                     </button>
                                                     <div className="wishlist">
